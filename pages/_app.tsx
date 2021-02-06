a29f0a5cdbdd020e6e28991879fb8f2617ed3d93
t2 <oo_ri@naver.com>
2021-02-07 02:21:20 +0900

    Tailwind CSS Install and Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { memo } from 'react';
 import { AppProps } from 'next/app';
 
+import '../styles/styles.css';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
