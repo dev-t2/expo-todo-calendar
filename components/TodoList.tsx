1254088dbc6a2a31a543a5a9347dfe71ac098773
t2 <oo_ri@naver.com>
2021-02-11 03:33:33 +0900

    HeroIcons Tailwind CSS Update

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,4 +1,5 @@
 import { memo, useMemo } from 'react';
+
 import { ColorType, TodoType } from '../types/todo';
 
 interface IProps {
@@ -47,7 +48,39 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
                 </div>
 
                 <div className="flex mr-3">
-                  {!todo.checked && (
+                  {todo.checked ? (
+                    <>
+                      <svg
+                        className="w-6 mr-3 text-red-900"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                        />
+                      </svg>
+
+                      <svg
+                        className="w-6 text-green-900"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M5 13l4 4L19 7"
+                        />
+                      </svg>
+                    </>
+                  ) : (
                     <button
                       className="w-5 h-5 rounded-full border border-solid border-gray-200 bg-transparent focus:outline-none"
                       onClick={() => {}}
