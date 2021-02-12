7d4d1d9574150e670d12c8bbecaa8df553b82ee3
t2 <oo_ri@naver.com>
2021-02-12 16:58:45 +0900

    Redux Install and Setup

---
+++ store/todo.ts
@@ -0,0 +1,33 @@
+import { TodoType } from '../types/todo';
+
+export const INIT_TODO_LIST = 'todo/INIT_TODO_LIST';
+
+export const setTodo = (payload: TodoType[]) => {
+  return {
+    type: INIT_TODO_LIST,
+    payload,
+  };
+};
+
+export const todoActions = { setTodo };
+
+interface TodoReduxState {
+  todos: TodoType[];
+}
+
+const initialState: TodoReduxState = {
+  todos: [],
+};
+
+const reducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case INIT_TODO_LIST:
+      const newState = { ...state, todos: action.payload };
+
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
