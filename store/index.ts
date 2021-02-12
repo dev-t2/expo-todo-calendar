e8d98e92fe39d2d8cbfc11048b34f25b7821a4e9
t2 <oo_ri@naver.com>
2021-02-12 17:38:42 +0900

    Redux ToolKit Install and Setup

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper, HYDRATE } from 'next-redux-wrapper';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-
+import { combineReducers } from 'redux';
 import todo from './todo';
 
 const rootReducer = combineReducers({
-  todo,
+  todo: todo.reducer,
 });
 
 const reducer = (state: any, action: any) => {
@@ -14,6 +14,8 @@ const reducer = (state: any, action: any) => {
       ...action.payload,
     };
 
+    if (state.count) nextState.count = state.count;
+
     return nextState;
   }
 
@@ -22,18 +24,11 @@ const reducer = (state: any, action: any) => {
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-const bindMiddleware = (middleware: any) => {
-  if (process.env.NODE_ENV !== 'production') {
-    const { composeWithDevTools } = require('redux-devtools-extension');
-
-    return composeWithDevTools(applyMiddleware(...middleware));
-  }
-
-  return applyMiddleware(...middleware);
-};
-
 const initStore = () => {
-  return createStore(reducer, bindMiddleware([]));
+  return configureStore({
+    reducer,
+    devTools: true,
+  });
 };
 
 export const wrapper = createWrapper(initStore);
--- store/todo.ts
+++ store/todo.ts
@@ -1,15 +1,5 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TodoType } from '../types/todo';
-
-export const SET_TODO_LIST = 'todo/SET_TODO_LIST';
-
-export const setTodo = (payload: TodoType[]) => {
-  return {
-    type: SET_TODO_LIST,
-    payload,
-  };
-};
-
-export const todoActions = { setTodo };
 
 interface TodoReduxState {
   todos: TodoType[];
@@ -19,15 +9,16 @@ const initialState: TodoReduxState = {
   todos: [],
 };
 
-const reducer = (state = initialState, action: any) => {
-  switch (action.type) {
-    case SET_TODO_LIST:
-      const newState = { ...state, todos: action.payload };
+const todo = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    setTodo(state, action: PayloadAction<TodoType[]>) {
+      state.todos = action.payload;
+    },
+  },
+});
 
-      return newState;
-    default:
-      return state;
-  }
-};
+export const todoActions = { ...todo.actions };
 
-export default reducer;
+export default todo;
