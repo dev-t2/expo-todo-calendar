673ff52b60bc39d9c9470e311f1439cae9171b50
t2 <oo_ri@naver.com>
2022-06-20 11:59:10 +0900

    01-ipfs update.ts Update

--- 01-ipfs/src/upload.ts
+++ 01-ipfs/src/upload.ts
@@ -1,12 +1,17 @@
-import * as IPFS from 'ipfs-core';
-import fs from 'fs/promises';
+import { create } from 'ipfs-core';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 const main = async () => {
-  const ipfs = await IPFS.create();
-  const file = await fs.readFile('./file.txt');
-  const result = await ipfs.add(file);
+  try {
+    const ipfs = await create();
+    const file = await readFile(join(__dirname, 'file.txt'));
+    const result = await ipfs.add(file);
 
-  console.log(result);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 main();
