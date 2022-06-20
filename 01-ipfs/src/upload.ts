ff5630f1350457ad1da037967d1c308ee95bd3b6
t2 <oo_ri@naver.com>
2022-06-20 10:47:40 +0900

    01-ipfs ipfs-core Setup

--- 01-ipfs/src/upload.ts
+++ 01-ipfs/src/upload.ts
@@ -1,11 +1,10 @@
-import { create } from 'ipfs-http-client';
+import * as IPFS from 'ipfs-core';
 import fs from 'fs/promises';
 
-const client = create();
-
 const main = async () => {
+  const ipfs = await IPFS.create();
   const file = await fs.readFile('./file.txt');
-  const result = await client.add(file);
+  const result = await ipfs.add(file);
 
   console.log(result);
 };
