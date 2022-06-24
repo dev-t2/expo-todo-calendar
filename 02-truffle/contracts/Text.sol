8e4c8aef56d04254ca060ce601019c7a3bf20e90
t2 <oo_ri@naver.com>
2022-06-24 11:01:03 +0900

    02-truffle Text.sol Update

---
+++ 02-truffle/contracts/Text.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Text {
+    string public text;
+
+    constructor(string memory _text) {
+        text = _text;
+    }
+
+    function setText(string memory _text) public {
+        text = _text;
+    }
+
+    function getText() public view returns (string memory) {
+        return text;
+    }
+
+    function errorOccur(uint256 e) public pure returns (uint256) {
+        require(e == 0, "Error");
+
+        return e;
+    }
+}
