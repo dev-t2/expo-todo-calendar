d8a8e5751be631a0ed6a4fcafd951b3300863980
t2 <oo_ri@naver.com>
2022-06-26 07:31:30 +0900

    02-truffle Solidity Prettier Update

--- 02-truffle/contracts/Text.sol
+++ 02-truffle/contracts/Text.sol
@@ -2,23 +2,23 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Text {
-    string public text;
+  string public text;
 
-    constructor(string memory _text) {
-        text = _text;
-    }
+  constructor(string memory _text) {
+    text = _text;
+  }
 
-    function setText(string memory _text) public {
-        text = _text;
-    }
+  function setText(string memory _text) public {
+    text = _text;
+  }
 
-    function getText() public view returns (string memory) {
-        return text;
-    }
+  function getText() public view returns (string memory) {
+    return text;
+  }
 
-    function errorOccur(uint256 error) public pure returns (uint256) {
-        require(error == 0, "Error");
+  function errorOccur(uint256 error) public pure returns (uint256) {
+    require(error == 0, 'Error');
 
-        return error;
-    }
+    return error;
+  }
 }
